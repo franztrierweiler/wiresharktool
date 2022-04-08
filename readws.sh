9e2bf92f771d218d7c871f94f5e16637ff37fa46
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2022-04-08 19:10:31 +0200

    Retrait de commentaires

--- readws.sh
+++ readws.sh
@@ -8,8 +8,6 @@ for file in `ls input_files`
 do
 	echo "Analyse de $file"
 	ls input_files/$file
-	# tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep notAfter -A 2 | grep utcTime: >>date_out.txt
-	# tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" | grep "0000" | cut -c1-2
 	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" | grep "0000" | cut -c67-82 >>serial_numbers.txt
 	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" -A 100 | grep "validity" -A 5 | grep notAfter -A 2 | grep utcTime: >>exp_dates.txt
 done
