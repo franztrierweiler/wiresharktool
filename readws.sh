0665f9f9dc78341926bed9aec445702b866375c5
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2022-04-08 23:18:20 +0200

    Correction anomalie empêchant de lire la bonne date d'expiration du certificat terminal.

--- readws.sh
+++ readws.sh
@@ -14,5 +14,5 @@ do
 	echo "Analyse de $file"
 	ls input_files/$file
 	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" | grep "0000" | cut -c67-82 >>serial_numbers.txt
-	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" -A 100 | grep "validity" -A 5 | grep notAfter -A 2 | grep utcTime: >>exp_dates.txt
+	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" -B 20 | grep "validity" -A 5 | grep notAfter -A 2 | grep utcTime: >>exp_dates.txt
 done
