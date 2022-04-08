92302ea0e1c19565063f5acd2a49774ed1818085
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2022-04-08 17:31:27 +0200

    Ajout de la boucle de lecture des fichiers

--- readws.sh
+++ readws.sh
@@ -1,10 +1,8 @@
-D
-C
-D
-C
-D
-A
-B
-B
-
 #!/bin/bash
+
+for file in `ls input_files`
+do
+	echo "Analyse de $file"
+	ls input_files/$file
+	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep notAfter -A 2
+done
