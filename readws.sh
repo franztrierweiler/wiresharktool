2105015c4d083d64e88fb3d4ea29f155970720c9
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2022-04-15 21:41:00 +0200

    Ajout d'informations d'exécution.

--- readws.sh
+++ readws.sh
@@ -11,8 +11,10 @@ rm exp_dates.txt
 
 for file in `ls input_files`
 do
-	echo "Analyse de $file"
-	ls input_files/$file
+	echo "- Analyse de `ls -all input_files/$file`"
+	echo " -- Extraction des TID"
 	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" | grep "0000" | cut -c67-82 >>serial_numbers.txt
+	echo " -- Extraction des dates d'expiration"
 	tshark -r input_files/$file -Y "tls.handshake.certificate" -V | grep "rdnSequence: 1 item (id-at-commonName=0000" -B 20 | grep "validity" -A 5 | grep notAfter -A 2 | grep utcTime: >>exp_dates.txt
+	echo "- Fin d'analyse"
 done
